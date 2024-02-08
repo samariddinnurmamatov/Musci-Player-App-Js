153e5711555b5ecf95ba9e4d5e38a015a228cc39
samariddinnurmamatov <samariddinnurmamatov111@gmail.com>
2024-02-08 15:06:05 +0500

    Music-Player-App

---
+++ README.md
@@ -0,0 +1,16 @@
+Music Player App
+-------------
+
+- - - -image:: doc/_static/image1.png
+    :target: doc/_static/image1.png
+    :height: 300px
+
+- - - -image:: doc/_static/image1.png
+    :target: doc/_static/image1.png
+    :height: 300px
+
+## Music Player App 
+
+![Image 1](doc/_static/image1.png)
+
+![Image 2](doc/_static/image2.png)
---
+++ music.js
@@ -0,0 +1,23 @@
+let trackList = [
+    {
+      name: "Omon Omon",
+      path: "./music/music1.mp3",
+      img: "./images/img1.jpg",
+    },
+    {
+      name: "Ey Dil",
+      path: "./music/music2.mp3",
+      img: "./images/img2.jpg",
+    },
+    {
+      name: "Yok Yok",
+      path: "./music/music3.mp3",
+      img: "./images/img3.jpg",
+    },
+    {
+      name: "Sokun Tun",
+      path: "./music/music4.mp3",
+      img: "./images/img4.jpg",
+    },
+  ];
+  
